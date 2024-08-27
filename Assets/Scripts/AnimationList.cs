92724bfe0d0316856670b4493ecb5122a36c2746
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-08-27 18:20:06 -0500

    Added ppl_002 animation

--- Assets/Scripts/AnimationList.cs
+++ Assets/Scripts/AnimationList.cs
@@ -5,13 +5,13 @@ using System.Collections;
 
 public class AnimationList : MonoBehaviour
 {
-    public Sprite[] ppl_01, ppl_04, ppl_07, ppl_08, ppl_10, ppl_11, ppl_12, ppl_13, ppl_14, ppl_15, ppl_16, ppl_17, ppl_18, ppl_23, ppl_24, ppl_25;
+    public Sprite[] ppl_01, ppl_02, ppl_04, ppl_07, ppl_08, ppl_10, ppl_11, ppl_12, ppl_13, ppl_14, ppl_15, ppl_16, ppl_17, ppl_18, ppl_23, ppl_24, ppl_25;
     public Sprite[] pet_02, pet_03, pet_04, pet_05, pet_06, pet_07, pet_08;
     public Sprite [] pool_06, pool_09;
     private Sprite[][] peopleAnimations, petAnimations, poolAnimations;
 
     public void Start() {
-        peopleAnimations = new Sprite[16][] {ppl_01, ppl_04, ppl_07, ppl_08, ppl_10, ppl_11, ppl_12, ppl_13, ppl_14, ppl_15, ppl_16, ppl_17, ppl_18,ppl_23, ppl_24, ppl_25};
+        peopleAnimations = new Sprite[17][] {ppl_01, ppl_02, ppl_04, ppl_07, ppl_08, ppl_10, ppl_11, ppl_12, ppl_13, ppl_14, ppl_15, ppl_16, ppl_17, ppl_18,ppl_23, ppl_24, ppl_25};
         petAnimations = new Sprite[7][] {pet_02, pet_03, pet_04, pet_05, pet_06, pet_07, pet_08};
         poolAnimations = new Sprite[2][] {pool_06, pool_09};
     }
